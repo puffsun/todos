caf689a15062aee44fc01fb23d406c6b4de0e3a4
George Sun <sunwinner3@gmail.com>
2015-04-01 08:00:33 +0800

    Init commit, with bugs unresolved.

---
+++ app.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/russross/blackfriday"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	println("Go web server up and running...")
+	http.ListenAndServe(":9090", nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
+}
