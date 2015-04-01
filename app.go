c1b2e1d35c411c42de5a34c7c57e82921ed47b0e
George Sun <sunwinner3@gmail.com>
2015-04-01 20:56:10 +0800

    Refactoring the routing and handlers.

--- app.go
+++ app.go
@@ -6,6 +6,7 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	// Imported a package solely for side-effects
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/russross/blackfriday"
 	"net/http"
@@ -14,14 +15,15 @@ import (
 // Global variables
 var (
 	TODOS_PATH = "/api/todos"
+	TODO_PATH  = "/api/todos/{todoId}"
 	todos      = Todos{
-		Todo{Id: "1", Title: "Write presentation", Completed: false},
-		Todo{Id: "2", Title: "Host meetup", Completed: false},
+		Todo{Id: 1, Title: "Write presentation", Completed: false},
+		Todo{Id: 2, Title: "Host meetup", Completed: false},
 	}
 )
 
 type Todo struct {
-	Id        string `json:"id"`
+	Id        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
@@ -43,15 +45,15 @@ func main() {
 	api.Methods("GET").HandlerFunc(ApiHandler)
 
 	todos := router.Path(TODOS_PATH).Subrouter()
-	todos.Methods("GET").HandlerFunc(TodosGetHandler)
-	todos.Methods("POST").HandlerFunc(TodosPostHandler)
-	todos.Methods("DELETE").HandlerFunc(TodosDeleteHandler)
-	todos.Methods("PUT").HandlerFunc(TodosPutHandler)
+	todos.Methods("GET").HandlerFunc(TodosIndexHandler)
+
+	todoItem := router.Path(TODO_PATH).Subrouter()
+	todoItem.Methods("GET").HandlerFunc(TodoItemGetHandler)
+	todoItem.Methods("POST").HandlerFunc(TodosPostHandler)
+	todoItem.Methods("DELETE").HandlerFunc(TodosDeleteHandler)
+	todoItem.Methods("PUT").HandlerFunc(TodosPutHandler)
 
 	// Serve static files in public directory
-	//http.Handle("/", http.FileServer(http.Dir("public")))
-	//router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
-
 	middleware := negroni.New(negroni.NewRecovery(),
 		negroni.NewStatic(http.Dir("public")),
 		// Even more middleware
@@ -60,7 +62,6 @@ func main() {
 	middleware.UseHandler(router)
 
 	// Server started
-	//http.ListenAndServe(":9090", router)
 	middleware.Run(":9090")
 }
 
@@ -69,25 +70,30 @@ func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
 	rw.Write(markdown)
 }
 
-func TodosGetHandler(rw http.ResponseWriter, r *http.Request) {
+func TodosIndexHandler(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(todos)
 }
 
-func TodosPostHandler(rw http.ResponseWriter, r *http.Request) {
-	println("Post request accepted.")
+func TodoItemGetHandler(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoId := vars["todoId"]
+	fmt.Fprintln(rw, "Todo item get handler: ", todoId)
+}
 
+func TodosPostHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Post Handler")
 }
 
 func TodosPutHandler(rw http.ResponseWriter, r *http.Request) {
-	println("Put request accepted.")
+	fmt.Fprintln(rw, "Put Handler")
 }
 
 func TodosDeleteHandler(rw http.ResponseWriter, r *http.Request) {
-	println("Delete request accepted.")
+	fmt.Fprintln(rw, "Delete Handler")
 }
 
 func ApiHandler(rw http.ResponseWriter, r *http.Request) {
-	println("API request accepted.")
+	fmt.Fprintln(rw, "API Handler")
 }
 
 func NewDB() *sql.DB {
